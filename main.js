67a2d51cfa0f5cfad77235c514f2cff2b7bdd508
victor-code <vdcloayza@gmail.com>
2022-08-05 14:50:07 -0400

    new version recovery

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import javascriptLogo2 from "./src/public/img/javascript-programming-language.webp"
-import viteLogo from './src/public/vite.svg'
-import htmlLogo from "./src/public/img/html-icon.png"
-import { setupCounter } from './counter.js'
+// import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import javascriptLogo2 from "./src/public/img/javascript-programming-language.webp"
+// import viteLogo from './src/public/vite.svg'
+// import htmlLogo from "./src/public/img/html-icon.png"
+// import { setupCounter } from './counter.js'
 
 window.onload=()=>{
   var myVar;
@@ -39,73 +39,8 @@ window.onload=()=>{
 
 
 
-document.querySelector('#app').innerHTML = //html
-`
-  <div>
-  <div id="loader"></div>
-   
-
-      
-  
-
-
-
-
-        
-    <div style="display:none;" class="contenedorall">
-        <section id="imagenportada">
-            
-            <h1 id="h1portada">BIENVENIDOS A MI PRACTICA DE JAVASCRIPT</h1>
-            
-        </section>
-
-        <div style="display:none;" id="myDiv" class="animate-bottom">
-        <h2>Tada!</h2>
-            <p>espero disfrutes lo que ves &#128522;
-                sigue bajando hacia abajo </p>
-                
-          </div>
-          <br /><br />
-        <h2>CONTENIDO DEL CURSO</h2>
-        <p>Empezemos con la super portada con estilo periódico
-            que creé,y espero les guste  &#128522;
-        </p>
-        <p>toque la imagen para los cursos correspondientes</p>
-        <div class="contenedor_cursos">
-            
-            <a href="/htmlpages"
-            onclick="route()"
-            >
-            <!-- //  target="_blank" -->
-            <img src=${htmlLogo} class="logo" alt="Vite logo" />
-          </a>
-          <a href="/jspages" onclick="route()">
-            <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-          </a>
-          <h1>Hello Welcome!</h1>
-      <hr />
-        </div>
-        <br><br><br>
-        
-<!-- /* INICIO DE VITE HOME*/ -->
-    <div class="card">
-    <button id="counter" type="button"></button>
-    <header>
-    <div class="app2"></div>
-    </header>
-  </div>
-
-
-  </div>
-  
-  <p class="read-the-docs">
-    Hola  ---------  querido ----  visitante
-  </p>
-    </div> 
-    <!-- FIN DE CONTENEDOR -->
-
-
-
-`
+// document.querySelector('#app').innerHTML = //html
+// `
+// `
 
 setupCounter(document.querySelector('#counter'))
