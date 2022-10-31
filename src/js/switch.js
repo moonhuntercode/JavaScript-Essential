e7ac63df9ebf4923e03e847001b8cd77a34d54cb
victor-code <vdcloayza@gmail.com>
2022-10-30 23:42:01 -0400

    switch statement added

--- src/js/switch.js
+++ src/js/switch.js
@@ -1,6 +1,20 @@
+    function toggle(ID) {
+        const button=document.getElementById('button1');
+        var demo=document.getElementById(ID)
+        if (demo.style.display=="none") {
+            button.innerHTML="hide syntax"
+            demo.style.display="block";
+        }
+     else if (demo.style.display="block"){
+            button.innerHTML="show syntax";
+            demo.style.display="none";
+        }
+    }
+
+//problem #2
 let edad=20
 let mensaje=""
-switch(edad){
+switch(edad ){
     case 14:mensaje="mensaje: eres menor de edad"
     break;
     case 20:mensaje="eres mayor de 19"
